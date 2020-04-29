7e501106a4a6af145efa5eb82081aaab8c9864b4
favour vivian woka <vivianemma97@gmail.com>
2020-04-29 17:16:18 +0100

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { ApiService } from "../api.service";
-import * as Chartist from "chartist";
-import { ChartType, ChartEvent } from "ng-chartist";
-declare var require: any;
-
-const data: any = require("./data.json");
-
-export interface Chart {
-  type: ChartType;
-  data: Chartist.IChartistData;
-  options?: any;
-  responsiveOptions?: any;
-  events?: ChartEvent;
-}
 
 @Component({
   selector: "app-dashboard",
@@ -20,66 +7,40 @@ export interface Chart {
   styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
-  data;
+  blogData;
+  page = 1;
+  loading = false;
   constructor(public apiService: ApiService) {}
   ngOnInit(): void {
+    this.getAllBlog();
+  }
+  getAllBlog() {
+    this.loading = true;
     this.apiService.getBlog().subscribe((response) => {
-      this.data = response;
       console.log(response);
+      this.loading = false;
+      this.blogData = response;
     });
   }
-
-  // Barchart
-  barChart1: Chart = {
-    type: "Bar",
-    data: {
-      labels: ["Post", "categories", "Page", "Media", "Block", "Them"],
-      series: [
-        [9, 4, 11, 7, 10, 12],
-        [3, 2, 9, 5, 8, 10],
-      ],
-    },
-    options: {
-      seriesBarDistance: 15,
-      high: 12,
-
-      axisX: {
-        showGrid: false,
-        offset: 20,
-      },
-      axisY: {
-        showGrid: true,
-        offset: 40,
-      },
-      height: 360,
-    },
-
-    responsiveOptions: [
-      [
-        "screen and (min-width: 640px)",
-        {
-          axisX: {
-            labelInterpolationFnc: function (
-              value: number,
-              index: number
-            ): string {
-              return index % 1 === 0 ? `${value}` : null;
-            },
-          },
-        },
-      ],
-    ],
-  };
-
-  // This is for the donute chart
-  // donuteChart1: Chart = {
-  //   type: "Pie",
-  //   data: data["Pie"],
-  //   options: {
-  //     donut: true,
-  //     height: 260,
-  //     showLabel: false,
-  //     donutWidth: 20,
-  //   },
-  // };
+  combineSlug(slug, id) {
+    return `${slug}-${id}`;
+  }
+  next() {
+    this.apiService.nextUser((this.page += 1)).subscribe((data) => {
+      console.log(data);
+      if (data) {
+        this.blogData = data;
+      }
+    });
+  }
+  previous() {
+    if (this.page > 1) {
+      this.apiService.previousUser((this.page -= 1)).subscribe((data) => {
+        console.log(data);
+        if (data) {
+          this.blogData = data;
+        }
+      });
+    }
+  }
 }
